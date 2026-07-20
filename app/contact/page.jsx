40e594e7113f92c6372f58ade05e7e11aa09798d
karinagb <karin.gb27@gmail.com>
2026-07-20 16:31:52 -0600

    1st commit

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,10 +1,8 @@
-import Image from 'next/image';
-
-export default function Home() {
+export default function Contact() {
   return (
     <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <main className='flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
-        <h1>Home</h1>
+        <h1>Contact</h1>
       </main>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <header>
+        <nav className="flex space-x-2 justify-center">
+          <Link href="/">Home</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
+      </header>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );

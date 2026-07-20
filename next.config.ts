311eb2aaa2d2685416320b303ad52488e021658a
karinagb <karin.gb27@gmail.com>
2026-07-20 17:28:37 -0600

    remove prefix for css

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/my-next-testing-app',
-  assetPrefix: '/my-next-testing-app',
 };
 
 export default nextConfig;
